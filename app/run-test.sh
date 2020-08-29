5d468995f0f646f27cfade90464876aba967f964
Okke Formsma <okke@formsma.nl>
2020-08-29 13:33:25 +0200

    update run-test

--- app/run-test.sh
+++ app/run-test.sh
@@ -10,7 +10,7 @@ if [ -z "$1" ]; then
 	exit 1
 elif [ "$1" = "all" ]; then
 	echo "" > ./build/tests/pass-fail.log
-	find tests -name native_posix.keymap -exec dirname \{\} \; | xargs -l -P 4 ./run-test.sh
+	find tests -name native_posix.keymap -exec dirname \{\} \; | xargs -l -P 4 ./run-test.sh > ./build/tests/pass-fail.log
 	err=$?
 	sort -k2 ./build/tests/pass-fail.log
 	exit $err
@@ -21,13 +21,13 @@ echo "Running $testcase:"
 
 west build -d build/$testcase -b native_posix -- -DZMK_CONFIG=$testcase > /dev/null 2>&1
 if [ $? -gt 0 ]; then
-	echo "FAIL: $testcase did not build" >> ./build/tests/pass-fail.log
+	echo "FAIL: $testcase did not build"
 else
 	./build/$testcase/zephyr/zmk.exe | sed -e "s/.*> //" | tee build/$testcase/keycode_events_full.log | sed -n -f $testcase/events.patterns > build/$testcase/keycode_events.log
 	diff -au $testcase/keycode_events.snapshot build/$testcase/keycode_events.log
 	if [ $? -gt 0 ]; then
-		echo "FAIL: $testcase" >> ./build/tests/pass-fail.log
+		echo "FAIL: $testcase"
 	else
-		echo "PASS: $testcase" >> ./build/tests/pass-fail.log
+		echo "PASS: $testcase"
 	fi
 fi
